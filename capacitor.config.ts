9b9e94c3886a86f34de3c67f54c4379d1f3fac9f
GLFABILA <glfabila2@gmail.com>
2026-04-06 07:40:12 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IT35A-MOBILE',
+  webDir: 'dist'
+};
+
+export default config;
