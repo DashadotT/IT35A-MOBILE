0540af92af03c33422fbf2505c5c2f42f68c79e1
DashadotT <markjovanoctat@gmail.com>
2026-04-06 08:24:23 +0800

    fixed app tsx error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import Home from './pages/Home';  
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,13 +34,15 @@ import '@ionic/react/css/palettes/dark.system.css';
 import './theme/variables.css';
 import Template from './pages/Template';
 
+
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/"> component={Template}></Route>
+        <Route exact path="/" component={Template}></Route>
+
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
