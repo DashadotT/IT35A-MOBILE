edf1424ba4d9879ed4f351044960314e87ddac3c
DashadotT <markjovanoctat@gmail.com>
2026-04-06 15:40:58 +0800

    Modified the Login.tsx to render the Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,26 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonButton, useIonRouter } from "@ionic/react";
 
 const Login: React.FC = () => {
     const navigation = useIonRouter();
 
-    const doLogin = () => {
-        navigation.push("/app", "forward", "replace");
-    };
+    const dologin = () => {
+        navigation.push('/app', 'forward', 'replace');
+    }
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
-                    <IonButtons>
-                        <IonMenuButton></IonMenuButton>
-                    </IonButtons>
-                    <IonTitle>My App</IonTitle>
+                    <IonTitle>Login</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
+                <IonButton expand="full" onClick={() => dologin()}>
+                    Login
+                </IonButton>
+
+
             </IonContent>
         </IonPage>
     );
-};
-
+}
 export default Login;
